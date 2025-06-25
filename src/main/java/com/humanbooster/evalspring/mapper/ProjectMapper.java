9cfefa79717f0f9c3c261bd8b630916e6757d8ad
JeromePrevotat <jer.prevotat@gmail.com>
2025-06-25 11:38:23 +0200

    Mapper done

--- src/main/java/com/humanbooster/evalspring/mapper/ProjectMapper.java
+++ src/main/java/com/humanbooster/evalspring/mapper/ProjectMapper.java
@@ -1,5 +1,54 @@
 package com.humanbooster.evalspring.mapper;
 
-public class ProjectMapper {
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Component;
+
+import com.humanbooster.evalspring.dto.ProjectDTO;
+import com.humanbooster.evalspring.model.Project;
+import com.humanbooster.evalspring.model.Task;
+import com.humanbooster.evalspring.repository.TaskRepository;
+import com.humanbooster.evalspring.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class ProjectMapper {
+    private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
+
+    public ProjectDTO toDTO(Project project) {
+        if (project == null) return null;
+        ProjectDTO projectDTO = new ProjectDTO();
+        projectDTO.setId(project.getId());
+        projectDTO.setName(project.getName());
+        projectDTO.setCreatorId(project.getCreator() != null ? 
+                                project.getCreator().getId() 
+                                : null);
+        projectDTO.setTasksListIds(project.getTasksList() != null ?
+                                     project.getTasksList().stream()
+                                             .map(Task::getId)
+                                             .collect(Collectors.toList())
+                                     : new ArrayList<>());
+        return projectDTO;
+    }
+
+    public Project toEntity(ProjectDTO projectDTO) {
+        if (projectDTO == null) return null;
+        Project project = new Project();
+        project.setId(projectDTO.getId());
+        project.setName(projectDTO.getName());
+        project.setCreator(userRepository.findById(projectDTO.getCreatorId()).orElse(null));
+        project.setTasksList(projectDTO.getTasksListIds() != null ?
+                             projectDTO.getTasksListIds().stream()
+                                    .map(taskRepository::findById)
+                                    .filter(Optional::isPresent)
+                                    .map(Optional::get)
+                                    .collect(Collectors.toList())
+                             : new ArrayList<>());
+        return project;
+    }
 }
--- src/main/java/com/humanbooster/evalspring/mapper/TaskMapper.java
+++ src/main/java/com/humanbooster/evalspring/mapper/TaskMapper.java
@@ -1,5 +1,44 @@
 package com.humanbooster.evalspring.mapper;
 
-public class TaskMapper {
+import org.springframework.stereotype.Component;
 
+import com.humanbooster.evalspring.dto.TaskDTO;
+import com.humanbooster.evalspring.model.Task;
+import com.humanbooster.evalspring.repository.ProjectRepository;
+import com.humanbooster.evalspring.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class TaskMapper {
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+    
+    public TaskDTO toDTO(Task task) {
+        if (task == null) return null;
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setId(task.getId());
+        taskDTO.setTitle(task.getTitle());
+        taskDTO.setStatus(task.getStatus());
+        taskDTO.setProjectId(task.getProject() != null ?
+                            task.getProject().getId()
+                            : null);
+        taskDTO.setAssigneeId(task.getAssignee() != null ?
+                            task.getAssignee().getId()
+                            : null);
+        return taskDTO;
+    }
+
+    public Task toEntity(TaskDTO taskDTO) {
+        if (taskDTO == null) return null;
+        Task task = new Task();
+        task.setId(taskDTO.getId());
+        task.setTitle(taskDTO.getTitle());
+        task.setStatus(taskDTO.getStatus());
+        if (taskDTO.getProjectId() != null) task.setProject(projectRepository.findById(taskDTO.getProjectId()).orElse(null));
+        task.setAssignee(userRepository.findById(taskDTO.getAssigneeId()).orElse(null));
+        return task;
+    }
+    
 }
