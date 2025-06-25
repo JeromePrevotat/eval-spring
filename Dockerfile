211d46fff077610082a708dd4fc0bd034d7eaa75
JeromePrevotat <jer.prevotat@gmail.com>
2025-06-25 10:24:58 +0200

    User model

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN mvn clean package -DskipTests
 # FROM eclipse-temurin:21
 FROM eclipse-temurin:21-jre
 WORKDIR /app
-COPY --from=build /app/target/taskManagement-0.0.1-SNAPSHOT.jar app.jar
+COPY --from=build /app/target/evalspring-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT [ "java", "-jar", "app.jar" ]   
 # CMD [ "java", "-jar", "app.jar" ]   
 
--- src/main/java/com/humanbooster/evalspring/model/User.java
+++ src/main/java/com/humanbooster/evalspring/model/User.java
@@ -1,5 +1,44 @@
 package com.humanbooster.evalspring.model;
 
-public class User {
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @NotBlank(message = "Username cannot be blank")
+    @Column(name = "username")
+    String username;
+
+    @OneToMany(mappedBy = "user",
+               cascade = CascadeType.ALL,
+               orphanRemoval = true)
+    @Column(name = "projects")
+    List<Project> projectsList;
+
+    @OneToMany(mappedBy = "user",
+               cascade = CascadeType.ALL,
+               orphanRemoval = true)
+    @Column(name = "tasks")
+    List<Task> tasksList;
 }
